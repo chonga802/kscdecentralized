ab23c4600761a8b313637382033bac1cdd10e8e9
Sage Price <sage.price@yale.edu>
2014-12-13 21:04:23 -0500

    Sage: Further work on Chord, some testing. Everything seems in order, need to add giving files to other nodes when a new predecessor appears.

--- ChordDHT.cc
+++ ChordDHT.cc
@@ -4,59 +4,34 @@
 #include <QStringList>
 #include <QCryptographicHash>
 
-void ChordDHT::initDHT(QString id) {
-	int i;
+ChordDHT::ChordDHT(QString id) {
+	qDebug() << "Creating Chord table for" << id;
+	int i, two;
 
 	myID = id;
 	myLoc = getLocation(id);
+	qDebug() << "My location in table is" << myLoc;
 
+	/* initially, predecessor is self - only one in network */
+	predLoc = myLoc;
+
+	two = 1;
 	for (i = 0; i < 8; i++) {
 		finger[i] = myID;
+		qDebug() << "\tFinger" << i << "is" << id << "starts at" << (myLoc + two) % 256;
+		two *= 2;
 	}
 }
 
-/*
-	WARNING TO SAGE: TEST THIS THOROUGHLY
-	TEST
-	TEST
-	TEST
-	TEST
-	TEST
-	TEST
-	TEST
-	TEST
-	TEST
-	PLEASE
-*/
-QString ChordDHT::getTracker(QString name, QByteArray metaID) {
-	QPair<QString, QByteArray> file = QPair<QString, QByteArray>(name, metaID);
+/* seems to be working */
+void ChordDHT::addFile(QString name,
+		QByteArray metaID,
+		QString seed,
+		QByteArray bytes) {
 
-	/* Check if I have the file, return my ID if so */
-	if (fileHashMap.contains(file)) {
-		return myID;
-	}
-	else {
-		int fileLoc = getLocation(name);
-		// get distance between current location and target
-		fileLoc = (fileLoc - (256 + myLoc)) % 256;
+	QPair<QString, QByteArray> file;
 
-		// find nearest finger
-		int fin = 0;
-		int step = 1;
-		while (fileLoc > step) {
-			step *= 2;
-			fin++;
-		}
-		if (fileLoc == step)
-			return finger[fin];
-		else
-			return finger[fin - 1];
-	}
-}
-
-
-void ChordDHT::addFile(QString name, QByteArray metaID, QString seed, QByteArray bytes) {
-	QPair<QString, QByteArray> file = QPair<QString, QByteArray>(name, metaID);
+	file = QPair<QString, QByteArray>(name, metaID);
 
 	if (fileHashMap.contains(file))
 		qDebug() << "ERROR: File already inserted, overwriting";
@@ -65,12 +40,17 @@ void ChordDHT::addFile(QString name, QByteArray metaID, QString seed, QByteArray
 	fileHashMap.insert(file, bytes);
 }
 
+/* working */
+void ChordDHT::addSeed(QString name,
+		QByteArray metaID,
+		QString seed) {
 
-void ChordDHT::addSeed(QString name, QByteArray metaID, QString seed) {
-	QPair<QString, QByteArray> file = QPair<QString, QByteArray>(name, metaID);
+	QPair<QString, QByteArray> file;
+	QStringList seeds;
 
+	file = QPair<QString, QByteArray>(name, metaID);
 	// extract all current seeds for file
-	QStringList seeds = fileSeedMap.value(file);
+	seeds = fileSeedMap.value(file);
 
 	// append new seed to list
 	seeds.append(seed);
@@ -79,60 +59,166 @@ void ChordDHT::addSeed(QString name, QByteArray metaID, QString seed) {
 	fileSeedMap.insert(file, seeds);
 }
 
+/* TODO: TEST THE SHIT OUT OF THIS
+*
+*	- peer updating appears to work correctly
+*	- as does predecessor calculation
+*
+*	- need to do sharing files with predecessor
+*/
+void ChordDHT::updateFingers(QString peerID) {
+	int peerLoc, fingerLoc, startLoc, d, i;
 
-QStringList ChordDHT::getSeeders(QString name, QByteArray metaID) {
+	// get location of new peer in table
+	peerLoc = getLocation(peerID);
 
+//	qDebug() << "Checking fingers for new friend" << peerID << "at" << peerLoc;
+
+	if (peerLoc == myLoc) {
+		qDebug() << "NODE COLLISION: #FUCKED";
+		qDebug() << "\t" << myID << "," << peerID;
+	}
+
+	d = 1;
+	// for each finger
+	for (i = 0; i < 8; i++) {
+		// get where finger's arc starts
+		startLoc = (myLoc + d) % 256;
+		// find current successor
+		fingerLoc = getLocation(finger[i]);
+
+//		qDebug() << "finger" << i << ":" << startLoc << ", new:" << peerLoc << ", old:" << fingerLoc;
+		//qDebug() << "\told friend" << finger[i];
+		/*
+		* if start location and new peer collide, obviously peer
+		*	needs to occupy this spot in finger table, and it will
+		*	certainly not appear in any other fingers, so break
+		*/ 
+		if (startLoc == peerLoc) {
+			finger[i] = peerID;
+			break;
+		}
+		/* if finger < new peer < old finger successor, update */
+		else if ((startLoc < peerLoc) && (peerLoc < fingerLoc)) {
+			finger[i] = peerID;
+		}
+		/* if old finger successor < finger < new peer, update */
+		else if ((fingerLoc < startLoc) && (startLoc < peerLoc)) {
+			finger[i] = peerID;
+		}
+		/* if new peer < old finger successor < finger, update */
+		else if ((peerLoc < fingerLoc) && (fingerLoc < startLoc)) {
+			finger[i] = peerID;
+		}
+
+		/* update start location */
+		d *= 2;
+	}
+
+	// check if new peer is your immediate predecessor
+	if ((predLoc < peerLoc) && (peerLoc < myLoc))
+		predLoc = peerLoc;
+	else if ((peerLoc < myLoc) && (myLoc < predLoc))
+		predLoc = peerLoc;
+	else if ((myLoc < predLoc) && (predLoc < peerLoc))
+		predLoc = peerLoc;
+	else if (predLoc == myLoc)
+		predLoc = peerLoc;
+
+	// if so, then must pass it files
+	if (peerLoc == predLoc) {
+		/* TODO: what the above comment says */
+		;
+	}
+/*
+	qDebug() << "New state of" << myID;
+	qDebug() << "\tpredLoc is" << predLoc;
+	for (i = 0; i < 8; i++) {
+		qDebug() << "\tFinger" << i << ":" << finger[i];
+	}
+*/
+}
+
+/* seems to be working consistently, should test x-platform */
+int ChordDHT::getLocation(QString id) {
+
+// Adopted from
+// http://stackoverflow.com/questions/22000005/built-in-64-bit-hash-function-for-qstring
+
+	//Generate hash
+	QByteArray hash = QCryptographicHash::hash(id.toUtf8(), QCryptographicHash::Sha1);
+
+	// Set size of bytes
+	Q_ASSERT(hash.size() == 8);
+
+	// Extract bytes
+	QDataStream stream(hash);
+	qint64 a, b;
+	stream >> a >> b;
+
+//	qDebug() << id << "," << (((a ^ b) % 256) + 256) % 256;
+	// return XOR of bits, mod 256 = pow(2, 8)
+	return (((a ^ b) % 256) + 256) % 256;
+
+}
+
+/* TODO: TEST THIS THOROUGHLY
+*
+*	seems okay right now? tested on a couple cases
+*
+*/
+QString ChordDHT::getTracker(QString name,
+		QByteArray metaID) {
+
+	qDebug() << "FINDING:" << name;
+
+	QPair<QString, QByteArray> file;
+	int fileLoc, fin, step;
+
+	file = QPair<QString, QByteArray>(name, metaID);
+
+	/* Check if I have the file, return my ID if so */
+	if (fileHashMap.contains(file)) {
+		qDebug() << "\tI'VE GOT IT";
+		return myID;
+	}
+	else {
+		qDebug() << "\t I DON'T HAVE IT";
+		fileLoc = getLocation(name);
+		qDebug() << "IT'S AT" << fileLoc;
+		// get distance between current location and target
+		fileLoc = ((fileLoc + 256) - myLoc) % 256;
+
+		// find nearest finger, start at furthest and work down
+		fin = 7;
+		step = 128;
+		while (fileLoc < step) {
+			step /= 2;
+			fin--;
+		}
+		return finger[fin];
+	}
+}
+
+
+QStringList ChordDHT::getSeeders(QString name,
+		QByteArray metaID) {
+
+	// Check if we have file, return if so
 	if(fileSeedMap.contains(QPair<QString, QByteArray>(name, metaID)))
 		return fileSeedMap.value(QPair<QString, QByteArray>(name, metaID));
 
+	// else throw error, return empty list
 	else
 		qDebug() << "ERROR: file" << name << "with id" << metaID.toHex() << "not found.";
 
+	/* questionable return, should think about this */
 	return QStringList("");
 }
 
 
-int ChordDHT::getLocation(QString id) {
-	/* FUCK WE'RE GONNA HAVE TO MAKE A HASH FUNCTION DAMMIT
-		here's some sample code:
-http://stackoverflow.com/questions/22000005/built-in-64-bit-hash-function-for-qstring
-
-	should use QString::toUtf8() instead of the utf16 call
-	current test code below
-
-	int collisions[256];
-	for (int i = 0; i < 256; i++)
-		collisions[i] = 0;
-
-	for (int i = 0; i < 30; i++) {
-QString str = QString::number(i);
-QByteArray hash = QCryptographicHash::hash(
-    QByteArray::fromRawData((const char*)str.toUtf8(), str.length()*2),
-    QCryptographicHash::Sha1
-  );
-  Q_ASSERT(hash.size() == 8);
-  QDataStream stream(hash);
-  qint64 a, b;
-  stream >> a >> b;
-
-	collisions[(((a ^ b) % 256)  + 256) % 256]++;
-  //qDebug() << "Testing for 'Test'" << (((a ^ b) % 256)  + 256) % 256;
-
-}
-	int fuck;
-	fuck = 0;
-	for (int i = 0; i < 256; i++) {
-		if (collisions[i] > 1)
-			fuck += collisions[i] - 1;
-	}
-	qDebug() << "Collisions:" << fuck;
-
-	 */
-	return 0;
-}
-
-
-QByteArray ChordDHT::getBytes(QString name, QByteArray metaID) {
+QByteArray ChordDHT::getBytes(QString name,
+		QByteArray metaID) {
 
 	if(fileHashMap.contains(QPair<QString, QByteArray>(name, metaID)))
 		return fileHashMap.value(QPair<QString, QByteArray>(name, metaID));
--- ChordDHT.hh
+++ ChordDHT.hh
@@ -1,19 +1,67 @@
 #ifndef CHORD_DHT_HH
 #define CHORD_DHT_HH
 
+/***************************************************************
+*
+*	ChordDHT.hh
+*
+*	API and class definitions for Chord distributed hash table
+*
+*	Chord protocol defines a DHT wherein each node in the
+*	network contols all items which hash to a location between
+*	the node itself and the preceeding node. In our (KSC)
+*	implementation, this means that all relevant metadata for
+*	a file (i.e. blocklist hash and list of seeders) will be
+*	stored in the node immediately succeeding its location
+*	within the hash table.
+*
+***************************************************************/
+
 #include <QByteArray>
 #include <QString>
 #include <QMap>
 #include <QPair>
 
+/*
+	//TEST CODE FOR MAIN
+
+	ChordDHT fuckit("Sagey");
+
+	fuckit.updateFingers("Friend!");
+	fuckit.updateFingers("Enemy");
+	fuckit.updateFingers("Boss!");
+	fuckit.updateFingers("Rando!");
+	fuckit.updateFingers("mothafucka");
+	fuckit.updateFingers("t");
+
+	fuckit.addFile("pray", QByteArray(), "Myself", QByteArray());
+
+	qDebug() << "testing owned by" << fuckit.getTracker("testing", QByteArray());
+	qDebug() << "Space Jam owned by" << fuckit.getTracker("Space Jam", QByteArray());
+	qDebug() << "lolz owned by" << fuckit.getTracker("lolz", QByteArray());
+	qDebug() << "pray owned by" << fuckit.getTracker("pray", QByteArray());
+*/
+
 class ChordDHT
 {
 public:
 
-	/* Initializes new empty ChordDHT:
-		fills all finger entriew with given id
-		should only be called once at startup		*/
-	void initDHT(QString);
+	/* Constructs new empty ChordDHT:
+		fills all finger entriew with given id		*/
+	ChordDHT(QString);
+
+	/* Adds file to this nodes maps, should only be
+		called the first time a file is uploaded	*/
+	void addFile(QString, QByteArray, QString, QByteArray);
+
+	/* Adds node as a seed for specified file		*/
+	void addSeed(QString, QByteArray, QString);
+
+	/* Updates finger table with peerID				*/
+	void updateFingers(QString);
+
+	/* Returns index of given string in DHT			*/
+	int getLocation(QString);
 
 	/* Returns either predecessor in finger table of
 		file, or actual tracker if that is known
@@ -23,28 +71,22 @@ public:
 		as tracker for given file					*/
 	QString getTracker(QString, QByteArray);
 
-	/* Adds file to this nodes maps					*/
-	void addFile(QString, QByteArray, QString, QByteArray);
-
-	/* Adds node as a seed for specified file		*/
-	void addSeed(QString, QByteArray, QString);
-
 	/* Returns seeds for given file					*/
 	QStringList getSeeders(QString, QByteArray);
-
-	/* Returns index of given string in DHT			*/
-	int getLocation(QString);
 
 	/* Returns blocklist hash for given file		*/
 	QByteArray getBytes(QString, QByteArray);
 
 private:
 
-	/* Node's location in table */
+	/* Node's index in table */
 	int myLoc;
 
 	/* Node's id */
 	QString myID;
+
+	/* Node's predecessor */
+	int predLoc;;
 
 	/* Finger table:
 		each entry is the nearest successor to
